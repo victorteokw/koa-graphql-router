dba78c055703d416eeb0d61b76152a1f291de146
Zhang Kaiyu <yeannylam@gmail.com>
2018-07-03 21:02:56 +0800

    version 0.0.1

---
+++ index.js
@@ -0,0 +1,35 @@
+const { graphqlKoa, graphiqlKoa } = require('apollo-server-koa');
+const { makeExecutableSchema } = require('graphql-tools');
+const { mergeTypes } = require('merge-graphql-schemas');
+const { applyMiddleware } = require('graphql-middleware');
+const merge = require('lodash.merge');
+const map = require('lodash/map');
+const path = require('path');
+const fs = require('fs');
+const glob = require('glob');
+const Router = require('koa-router');
+
+module.exports = ({ schemaDir, resolverDir, middlewares }) => {
+
+  const schema = makeExecutableSchema({
+    typeDefs: mergeTypes(map(
+      glob.sync(path.join(schemaDir, '**/*.gql')),
+      (filename) => fs.readFileSync(filename).toString()
+    )),
+    resolvers: merge(
+      ...map(glob.sync(path.join(resolverDir, '**/*.js'), require))
+    )
+  });
+
+  applyMiddleware(schema, ...middlewares);
+
+  const handle = (ctx, next) => graphqlKoa({ schema, context: ctx })(ctx, next);
+
+  const router = Router();
+  router.post('/graphql', handle);
+  router.get('/graphql', handle);
+  router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }));
+
+  return router;
+
+};
